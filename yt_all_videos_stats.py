2b51fd28136ea0aa3edcc322273267deb247f6b1
HarishK <kondaharish7@gmail.com>
2024-12-29 18:55:46 +0530

    29dec2024 - included new logics

--- yt_all_videos_stats.py
+++ yt_all_videos_stats.py
@@ -1,29 +1,41 @@
 """
 Extract videos from a youtube channel
 """
-import json
 import pandas as pd
-import googleapiclient.discovery
-from yt_stats_pwds import *
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 2000)
+pd.set_option('max_colwidth', 10000)
+from datetime import *
+job_start_time = datetime.now()
 
-api_service_name = "youtube"
-api_version = "v3"
+from yt_authenticate_yt_api import *
+from yt_Authenticate_AWS import *
+from yt_stats_contexts import *
 
-yt_client = googleapiclient.discovery.build(api_service_name, api_version, developerKey = DEVELOPER_KEY)
+yt_client = get_yt_client()
 
-# yt_channel_ids_list = [{'channelId': 'UCWv7vMbMWH4-V0ZXdmDpPBA', 'channelTitle': 'Programming with Mosh'},{'channelId': 'UC8butISFwT-Wl7EV0hUK0BQ', 'channelTitle': 'freeCodeCamp.org'}]
-yt_channel_ids_list = [{'channelId': 'UC8butISFwT-Wl7EV0hUK0BQ', 'channelTitle': 'freeCodeCamp.org'}]
+search_query = "python"
+
+yt_channel_ids_list = [{'channelId': 'UCWv7vMbMWH4-V0ZXdmDpPBA', 'channelTitle': 'Programming with Mosh'}]
+
 # Extract all the videos from each channel id
 yt_topic_video_ids_list = []; next_pg_token1 = ""
 for value in yt_channel_ids_list:
-    print(value['channelId'])
+    # print(value['channelId'])
+    print(f"{datetime.now()} | Pulling all the videos on the topic from the Channel: {value['channelTitle']}. ", end="");log_time = datetime.now()
     while True:
-        yt_search_resp = yt_client.search().list(part='id,snippet', type='video', channelId=value['channelId'], pageToken=next_pg_token1, fields="prevPageToken,nextPageToken,items(id,snippet(title))", maxResults=50).execute()
-        # print(yt_search_resp)
+        yt_search_resp = yt_client.search().list(part='id,snippet',
+                                                 type='video',
+                                                 channelId=value['channelId'],
+                                                 pageToken=next_pg_token1,
+                                                 fields="prevPageToken,nextPageToken,items(id,snippet(title))",
+                                                 maxResults=50
+                                                 ).execute()
 
         for a in yt_search_resp['items']:
-            if "python" in a['snippet']['title'].lower():
-                yt_topic_video_ids_list.append([a['id']['videoId'], a['snippet']['title'], value['channelTitle']])
+            if search_query in a['snippet']['title'].lower():
+                yt_topic_video_ids_list.append([value['channelTitle'], a['id']['videoId'], a['snippet']['title']])
 
         try:
             if yt_search_resp['nextPageToken']:
@@ -31,7 +43,32 @@ for value in yt_channel_ids_list:
         except Exception as pg_token_err:
             # print("There isn't a next page token")
             break
-        # break
+        break #to limit to 1 loop
+    print(f"elapsed, {datetime.now() - log_time}")
 
-df_all_topic_videos = pd.DataFrame(yt_topic_video_ids_list, columns=['video_id','video_title','channel_title'])
-print(df_all_topic_videos)
+df_all_topic_videos = pd.DataFrame(yt_topic_video_ids_list, columns=['channel_title', 'video_id', 'video_title'])
+
+print(f"{datetime.now()} | Pulling the video stats from all the Channels. ", end="");log_time = datetime.now()
+yt_video_stats_list = []
+for yt_video_details in yt_topic_video_ids_list:
+    video_info_resp = yt_client.videos().list(
+                                                part='statistics',
+                                                fields="items(statistics(viewCount,likeCount))",
+                                                id=yt_video_details[1]
+                                            ).execute()
+    yt_video_stats_list.append(yt_video_details+[video_info_resp['items'][0]['statistics']['viewCount'],video_info_resp['items'][0]['statistics']['likeCount']])
+print(f"elapsed, {datetime.now() - log_time}")
+
+df_video_stats = pd.DataFrame(yt_video_stats_list, columns=['channel_title', 'video_id', 'video_title', 'View_cnt', 'Likes_Cnt']).drop_duplicates()
+convert_dict = {'View_cnt': int, 'Likes_Cnt': int}
+df_video_stats = df_video_stats.astype(convert_dict)
+# print(df_video_stats)
+
+df_video_stats_smry = df_video_stats.groupby('channel_title').aggregate({'video_id':'count','View_cnt':'sum', 'Likes_Cnt':'sum'}).reset_index()
+df_video_stats_smry['Likes_%'] = (df_video_stats_smry['Likes_Cnt'] / df_video_stats_smry['View_cnt']).round(2)
+df_video_stats_smry.columns = ['Channel_Title','Total_Videos','Total_views','Total_likes','Likes_%']
+print(df_video_stats_smry)
+
+print(f"\n{str('--')*10}\n{job_start_time} | {datetime.now()} | {datetime.now() - job_start_time}")
+
+# yt_channel_ids_list = [{'channelId': 'UCWv7vMbMWH4-V0ZXdmDpPBA', 'channelTitle': 'Programming with Mosh'},{'channelId': 'UC8butISFwT-Wl7EV0hUK0BQ', 'channelTitle': 'freeCodeCamp.org'}]
--- yt_fetch_topic_channels.py
+++ yt_fetch_topic_channels.py
@@ -11,11 +11,11 @@ from yt_stats_contexts import *
 
 yt_client = get_yt_client()
 
+search_query = "github actions tutorials"
 #Get the video id's list from the search query
-search_query = "javascript tutorials"
-total_search_videos = 500 #in the multiples of 50
-
+total_search_videos = 100 #in the multiples of 50
 yt_video_ids_list = []; next_pg_token = ""
+print(f"{datetime.now()} | Searching for the videos. ", end=""); log_time = datetime.now()
 for iter in range(1,int(total_search_videos/50)+1):
     # print(f"loop: {iter}, page:{next_pg_token}")
     try:
@@ -30,9 +30,11 @@ for iter in range(1,int(total_search_videos/50)+1):
         for a in yt_search_resp['items']:
             yt_video_ids_list.append(a['id']['videoId'])
     except Exception as yt_search_error:
-        print(f"{yt_search_error}")
+        print(f"{datetime.now()} | {yt_search_error}")
+print(f"elapsed, {datetime.now() - log_time}")
 
 # Extract Channels id's list from the resulted videos list
+print(f"{datetime.now()} | Pulling the distinct channel id's from the search result videos. ", end=""); log_time = datetime.now()
 yt_channel_ids_list = []
 for yt_video_id in yt_video_ids_list:
     channel_info_resp = yt_client.videos().list(
@@ -42,17 +44,15 @@ for yt_video_id in yt_video_ids_list:
                                             ).execute()
     yt_channel_ids_list.append(channel_info_resp['items'][0]['snippet'])
     # break
+print(f"elapsed, {datetime.now() - log_time}")
 
+print(f"{datetime.now()} | Saving the results to S3. ", end=""); log_time = datetime.now()
 df_topic_chnls = pd.DataFrame(yt_channel_ids_list).drop_duplicates()
 df_topic_chnls['search_topic'] = search_query
-# print(df_topic_chnls)
 
 topic_chnls_list_file = f"{search_query.replace(" ", "_")}.csv"
 topic_chnls_list_s3_key = f"s3://{aws_s3_bucket}/data/yt_search_queries/yt_channels_list/{topic_chnls_list_file}"
 df_topic_chnls.to_csv(topic_chnls_list_s3_key, index=False, storage_options={"key": root_user_access_key, "secret": root_user_sceret_key})
+print(f"elapsed, {datetime.now() - log_time}")
 
 print(f"\n{str('--')*10}\n{job_start_time} | {datetime.now()} | {datetime.now() - job_start_time}")
-
-
-# topic_chnls_s3_key_full = f"s3://{aws_s3_bucket}/data/yt_channels/full/search_query={search_query.replace(" ", "_")}/date={datetime.now().date()}/{topic_chnls_file}"
-# topic_chnls_s3_key_latest = f"s3://{aws_s3_bucket}/data/yt_channels/latest/search_query={search_query.replace(" ", "_")}/{search_query.replace(" ", "_")}.csv"
